fca3fc6b7dba702f4ee370541e79b7b8f1b27639
Chayapol Moemeng <mchayapol@gmail.com>
2019-12-01 18:28:14 +0700

    Add HLT RP

---
+++ hlt/rp/precache-manifest.b9d8f8532e5eb8bb6c3119f97bb7498d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9aea1bb91aed4518588a5dc03cabda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4053d9d9f779bbd8dc29",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5d8a73a4d787edb06a1a",
+    "url": "/static/css/main.ee1a2837.chunk.css"
+  },
+  {
+    "revision": "4053d9d9f779bbd8dc29",
+    "url": "/static/js/2.0b24e735.chunk.js"
+  },
+  {
+    "revision": "5d8a73a4d787edb06a1a",
+    "url": "/static/js/main.eeec7299.chunk.js"
+  },
+  {
+    "revision": "ab7fd612ef567d1a50e8",
+    "url": "/static/js/runtime-main.a0938a23.js"
+  }
+]);
