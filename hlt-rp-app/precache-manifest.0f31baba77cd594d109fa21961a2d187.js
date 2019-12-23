981d2d118450301643c7b6e5dd198adcf9abf575
Chayapol Moemeng <mchayapol@gmail.com>
2019-12-23 15:06:30 +0700

    Update 1.0.4

---
+++ hlt-rp-app/precache-manifest.0f31baba77cd594d109fa21961a2d187.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebd5bb6d2d17741b296e3d1034c08e4",
+    "url": "/hlt-rp-app/index.html"
+  },
+  {
+    "revision": "eee950197960b1af1892",
+    "url": "/hlt-rp-app/static/css/2.293d0086.chunk.css"
+  },
+  {
+    "revision": "9ef8a5e64b81e62dc6e1",
+    "url": "/hlt-rp-app/static/css/main.84567492.chunk.css"
+  },
+  {
+    "revision": "eee950197960b1af1892",
+    "url": "/hlt-rp-app/static/js/2.277324f9.chunk.js"
+  },
+  {
+    "revision": "211956c46c3a52d1197bec93bf78a2f4",
+    "url": "/hlt-rp-app/static/js/2.277324f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ef8a5e64b81e62dc6e1",
+    "url": "/hlt-rp-app/static/js/main.5d8c4faa.chunk.js"
+  },
+  {
+    "revision": "69c3bacd9959bbf0b73c",
+    "url": "/hlt-rp-app/static/js/runtime-main.1f523fe0.js"
+  }
+]);
