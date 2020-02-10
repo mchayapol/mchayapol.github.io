69046ff892476be2b3ad6069885ce597a3f23f7b
Chayapol Moemeng <mchayapol@gmail.com>
2020-02-10 14:13:54 +0700

    add sp-app

---
+++ hlt-sp-app/precache-manifest.f8e7291624f60861518a25a655194432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c268270716a887e3745cb954c86683",
+    "url": "/hlt-sp-app/index.html"
+  },
+  {
+    "revision": "74ada800e91ee9da1414",
+    "url": "/hlt-sp-app/static/css/2.06f32ede.chunk.css"
+  },
+  {
+    "revision": "c970395ad207b86eb017",
+    "url": "/hlt-sp-app/static/css/main.d0bb15a2.chunk.css"
+  },
+  {
+    "revision": "74ada800e91ee9da1414",
+    "url": "/hlt-sp-app/static/js/2.e1dbb814.chunk.js"
+  },
+  {
+    "revision": "c970395ad207b86eb017",
+    "url": "/hlt-sp-app/static/js/main.be1715c0.chunk.js"
+  },
+  {
+    "revision": "c2fbcd52805e9012b8f4",
+    "url": "/hlt-sp-app/static/js/runtime~main.a067aeed.js"
+  }
+]);
